df04edb290f4de606b29a8a4c2f0e6d111c67950
Matt Kruse <github@mattkruse.com>
2017-08-25 12:40:57 -0500

    bug fixes

--- public_html/js/battlescripts-app.js
+++ public_html/js/battlescripts-app.js
@@ -494,6 +494,10 @@ bsapp.factory('$battlescripts', ["$firebaseArray", "$firebaseObject","$firebaseA
           return (typeof changed_player_move!=="undefined")?changed_player_move:player_move;
         });
     };
+    this.start_game = function(data) { return this.player.start_game(data); };
+    this.end_game = function(data) { return this.player.end_game(data); };
+    this.start_match = function(data) { return this.player.start_match(data); };
+    this.end_match = function(data) { return this.player.end_match(data); };
     this.error = function(err) {
       console.log(err);
       $rootScope.$broadcast("error/player",err);
